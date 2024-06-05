2255ddee12bc0f217546d1d0f12da910f84ce967
UGOOD <tyler.oliverjames@gmail.com>
2024-06-06 09:59:40 +1000

    added toggle for menu

---
+++ app/components/EmailButton.js
@@ -0,0 +1,45 @@
+"use client";
+import React, { useEffect, useRef } from 'react';
+
+const EmailButton = () => {
+   const buttonRef = useRef(null);
+
+   useEffect(() => {
+      const button = buttonRef.current;
+
+      const copyToClipboard = async () => {
+         const text = "help@blueroomstudios.com.au"; // Replace this with the text you want to copy
+
+         try {
+            if (navigator.clipboard && navigator.clipboard.writeText) {
+               await navigator.clipboard.writeText(text);
+               alert('Email: help@blueroomstudios.com.au - Copied to clipboard!');
+            } else {
+               const textArea = document.createElement('textarea');
+               textArea.value = text;
+               document.body.appendChild(textArea);
+               textArea.select();
+               document.execCommand('copy');
+               document.body.removeChild(textArea);
+               alert('Copied email to clipboard!');
+            }
+         } catch (err) {
+            console.error('Failed to copy: ', err);
+         }
+      };
+
+      button.addEventListener('click', copyToClipboard);
+
+      return () => {
+         button.removeEventListener('click', copyToClipboard);
+      };
+   }, []);
+
+   return (
+      <div ref={buttonRef} className="email link">
+         <p>[contact]</p>
+      </div>
+   );
+};
+
+export default EmailButton;
--- app/components/Menu.js
+++ app/components/Menu.js
@@ -1,16 +1,44 @@
+"use client";
 import About from "./About";
+import EmailButton from "./EmailButton";
+import EmailButtonFooter from "./EmailButtonFooter";
 import Services from "./Services";
+import { useState } from "react";
 
 export default function Menu() {
+
+   const [isAboutOpen, setAboutOpen] = useState(false);
+   const [isServicesOpen, setServicesOpen] = useState(false);
+
+   const handleAboutToggle = () => {
+      if (isAboutOpen) {
+         setAboutOpen(false);
+      } else {
+         setAboutOpen(true);
+      }
+   }
+
+   const handleServicesToggle = () => {
+      if (isServicesOpen) {
+         setServicesOpen(false);
+      } else {
+         setServicesOpen(true);
+      }
+   }
+
     return (
         <>
             <div className="menuOpen">
-                <p className="menuItem menuInactive">[about]</p>
-                <p className="menuItem">[services]</p>
-                <p className="menuItem menuInactive">[contact]</p> {/* Make this copy email */}
+                <p className="menuItem about"
+                  onClick={handleAboutToggle}>
+                     [about]</p>
+                <p className="menuItem services"
+                  onClick={handleServicesToggle}>
+                     [services]</p>
+                <EmailButton />
             </div>
-            {/* <Services /> */}
-            <About />
+            {isAboutOpen ? ( <About /> ) : ( null )}
+            {isServicesOpen ? ( <Services /> ) : ( null )}
             <div className="menuFooter">
                 <div className="themeButtons">
                     <p className="themeItem">[blue]</p>
@@ -18,7 +46,7 @@ export default function Menu() {
                     <p className="themeItem menuInactive">[black]</p>
                 </div>
                 <div className="contactButton">
-                    <p className="contactItem">[help@blueroomstudios.com.au]</p>
+                    <EmailButtonFooter />
                 </div>
             </div>
         </>
