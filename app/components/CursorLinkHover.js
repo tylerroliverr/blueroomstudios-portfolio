22f540127fc47420e6f2dabdb8ba4c94b0c8ff48
tylerroliverr <tyler.oliverjames@gmail.com>
2024-07-08 16:19:27 +1000

    removed link hover for the meantime

--- app/components/CursorLinkHover.js
+++ app/components/CursorLinkHover.js
@@ -7,27 +7,27 @@ export default function CursorHoverLink() {
         const link = document.querySelectorAll('.link');
         const navLink = document.querySelectorAll('.navLink');
 
-        link.forEach(function (links) {
-            links.addEventListener('mouseover', function () {
-                customCursor.classList.add('hovering');
-                customPointer.classList.add('hovering');
-            });
+        // link.forEach(function (links) {
+        //     links.addEventListener('mouseover', function () {
+        //         customCursor.classList.add('hovering');
+        //         customPointer.classList.add('hovering');
+        //     });
         
-            links.addEventListener('mouseout', function () {
-                customCursor.classList.remove('hovering');
-                customPointer.classList.remove('hovering');
-            });
-        });
+        //     links.addEventListener('mouseout', function () {
+        //         customCursor.classList.remove('hovering');
+        //         customPointer.classList.remove('hovering');
+        //     });
+        // });
 
-        navLink.forEach(function (navlinks) {
-            navlinks.addEventListener('mouseover', function () {
-                customPointer.classList.add('hoveringNav');
-            });
+        // navLink.forEach(function (navlinks) {
+        //     navlinks.addEventListener('mouseover', function () {
+        //         customPointer.classList.add('hoveringNav');
+        //     });
         
-            navlinks.addEventListener('mouseout', function () {
-                customPointer.classList.remove('hoveringNav');
-            });
-        });
+        //     navlinks.addEventListener('mouseout', function () {
+        //         customPointer.classList.remove('hoveringNav');
+        //     });
+        // });
 
     },[]);
 }
