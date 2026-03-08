99c28114e6fb25798cabf14141a3e1edc9176a6d
Tyler Oliver <tyler.oliverjames@gmail.com>
2026-03-08 15:31:08 +1100

    updated

--- app/components/ProjectNav.js
+++ app/components/ProjectNav.js
@@ -42,8 +42,6 @@ export default async function ProjectNav() {
           <p>Blueroom studios is a creative agency specialising in website design and development. Run by Tyler as a freelance agency from Melbourne, Naarm. Blueroom aims to create beautiful digital online works through these mediums to portray brands, portfolios and the like in a unique way.</p>
           <br></br>
           <p>In the realm of web design, Tylers journey spans over a decade. Bringing a unique touch to each project, combining a love ❦ for <span className={styles.soft}>soft</span>, <span className={styles.bold}>bold</span>, and <span className={styles.minimal}>minimal</span> aesthetics.</p>
-          <br></br>
-          <p>More than just designing and building websites, blueroom crafts digital experiences that bring those genuine wow moments.</p>
         </div>
 
         <div className={styles.projectContainer}>
